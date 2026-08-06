99df0e52149e4063445c8d06eaa4c8680092db6c
IanGatu <gatuian110@gmail.com>
2026-08-06 12:30:02 +0300

    modified the exceptionhandler to output a custom error json

--- src/main/java/com/example/customerapp/globalexceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/example/customerapp/globalexceptionhandler/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.example.customerapp.globalexceptionhandler;
 
+import java.time.LocalDateTime;
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.example.customerapp.apierror.ApiError;
 import com.example.customerapp.customernotfoundexception.CustomerNotFoundException;
 
 @ControllerAdvice
@@ -12,9 +16,18 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomerNotFoundException.class)
 
-        public ResponseEntity<Object> handleCustomerNotFound(CustomerNotFoundException ex){
+        public ResponseEntity<ApiError> handleCustomerNotFound(CustomerNotFoundException ex,HttpServletRequest request){
 
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+             ApiError error = new ApiError(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                request.getRequestURI());
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(error);
         }
     
 }
